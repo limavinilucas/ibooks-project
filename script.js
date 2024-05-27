2e64d70b842f3b2e4a4781915f5857b76014341c
Lucas Vinicius <137515842+limavinilucas@users.noreply.github.com>
2024-05-26 23:27:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,168 @@
+const bests = [ // Array de Objetos
+    {
+        name : "Harry Potter - A Série",
+        image : "harrypotter.webp",
+        description : "Harry Potter é um garoto órfão que vive infeliz com seus tios, os Dursleys. Ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Harry adentra um mundo mágico que jamais imaginara, vivendo diversas aventuras com seus novos amigos, Rony Weasley e Hermione Granger.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "O Poder do Hábito",
+        image : "O_Poder_Do_Habito.jpg",
+        description : "O Poder do Hábito: por que fazemos o que fazemos na vida e nos negócios é um livro de Charles Duhigg, ex-repórter do New York Times, publicado em Fevereiro de 2012 pela Random House. Explora a ciência por trás da criação e reforma de hábitos. O livro chegou à lista de mais vendidos do New York Times, Amazon.com e USA Today.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Guerra dos Tronos",
+        image : "guerradostronos.jpg",
+        description : "A Game of Thrones é o primeiro livro da série de fantasia épica As Crônicas de Gelo e Fogo, escrita pelo norte-americano George R. R. Martin e publicada pela editora Bantam Spectra. A novela Blood of the Dragon, compreendendo os capítulos de Daenerys Targaryen do romance, venceu o Prêmio Hugo de 1997 para 'Melhor Novela'.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Duna",
+        image : "Duna.jpg",
+        description : "Uma estonteante mistura de aventura e misticismo, ecologia e política, este romance ganhador dos prêmios Hugo e Nebula deu início a uma das mais épicas histórias de toda a ficção científica. o jovem Paul Atreides, herdeiro de uma poderosa família que foi vítima de um golpe, é treinado nas doutrinas secretas de uma antiga irmandade, que vê nele a esperança de realização de um plano urdido há séculos.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Entendendo Algoritmos",
+        image : "entendendoalgoritmos.jpg",
+        description : "Um guia ilustrado para programadores e outros curiosos. Um algoritmo nada mais é do que um procedimento passo a passo para a resolução de um problema. Os algoritmos que você mais utilizará como um programador já foram descobertos, testados e provados. Se você quer entendê-los, mas se recusa a estudar páginas e mais páginas de provas, este é o livro certo.",
+        repository : "secaoVendas.html"
+    }
+];
+
+const romances = [ // Array de Objetos
+    {
+        name : "Mecânica do Amor",
+        image : "A_Mecânica_Do_Amor.jpg",
+        description : "Um romance geek da mesma autora do best-seller fenômeno do TikTok A sociedade de AtlasBel não sabe o que fazer quando se formar no colégio e nem quer pensar no assunto. Mas quando revela um talento especial para engenharia, é incentivada por uma professora a entrar para o clube de robótica da escola.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "A Culpa é das Estrelas",
+        image : "aculpaedasestrelas.jpg",
+        description : "Hazel Grace é uma jovem prestes a completar dezessete anos de idade e desde os treze anos sofre com um cancro na tiroide, que evoluiu para uma metástase no pulmão e faz com que ela tenha que andar com um cilindro de oxigênio e uma cânula no nariz para conseguir respirar. A mãe conclui que ela estava deprimida, já que passava muito tempo pensando na morte.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "A Hipótese do Amor",
+        image : "hipotesedoamor.jpg",
+        description : "Quando um namoro de mentira entre cientistas encontra a irresistível força da atração, todas as teorias cuidadosamente calculadas sobre o amor são postas à prova. Com personagens cativantes e diálogos afiados, este livro engraçado, sexy e inteligente se tornou uma das grandes sensações do TikTok.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Orgulho e Preconceito",
+        image : "orgulho_e_preconceito.jpg",
+        description : "Pride and Prejudice (br/pt: Orgulho e Preconceito) é um romance da escritora britânica Jane Austen. Publicado pela primeira vez em 1813, na verdade havia sido terminado em 1797,[4] antes de ela completar 21 anos, em Steventon, Hampshire, onde Jane morava com os pais. Originalmente denominado First Impressions, nunca foi publicado sob aquele título; ao fazer a revisão dos escritos, Jane intitulou a obra e a publicou como Pride and Prejudice.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Heartstopper",
+        image : "heartstopper.jpg",
+        description : "Heartstopper é um romance gráfico britânico de drama adolescente escrito e ilustrado por Alice Oseman. A webcomic foi adaptada na série homônima dirigida por Euros Lyn e lançada em 2022, a série é um original da Netflix e até o momento tem 2 temporadas, e mais uma confirmada.",
+        repository : "secaoVendas.html"
+    }
+];
+
+const fantasy = [
+    {
+        name : "A Rainha Vermelha",
+        image : "rainhavermelha.jpg",
+        description : "Mare Barrow é uma Vermelha que vive com os pais e a irmã mais nova, Gisa; enquanto seus três irmãos mais velhos, Bree, Tramy e Shade estão atualmente servindo no front de uma guerra entre o Reino de Lakeland e o Reino de Norta, que é onde os Barrow's moram e que é governado pelo rei Tiberias Calore VI, um Prateado com poderes de fogo.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Os Jogos Vorazes",
+        image : "hungergames.jpg",
+        description : "Narrado em primeira pessoa, o livro acompanha Katniss Everdeen, uma garota de dezesseis anos que vive em um país distópico chamado Panem - localizado onde estaria a América do Norte. O país é dominado por uma metrópole tecnologicamente avançada chamada Capital, que realiza anualmente os Jogos Vorazes, para que as pessoas sempre se lembrem da revolta que ocorreu décadas atrás.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "O Hobbit",
+        image : "hobbit.webp",
+        description : "Situado em um tempo 'Entre o Alvorecer das Fadas e o Domínio dos Homens', o livro segue a busca do hobbit caseiro Bilbo Bolseiro para conquistar uma parte do tesouro guardado pelo dragão Smaug. A jornada de Bilbo o leva de um ambiente rural alegre a um território mais sinistro.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Coraline",
+        image : "coraline.jpg",
+        description : "Coraline é uma novela fantástica de terror do autor britânico Neil Gaiman, publicado em 2002 pela Bloomsbury, no Reino Unido e pela Harper Collins, nos Estados Unidos. Ganhou os prêmios Hugo e Nebula Award de melhor livro dos anos 2000 e o Bram Stoker Award de melhor trabalho de novos escritores de 2002.",
+        repository : "secaoVendas.html"
+    },
+    {
+        name : "Percy Jackson e o Ladrão de Raios",
+        image : "percyjackson.webp",
+        description : "Os deuses do Olimpo continuam vivos, em pleno século XXI! Eles ainda se apaixonam por mortais e têm filhos que podem se tornar grandes heróis, mas que acabam, na maioria das vezes, encontrando destinos terríveis nas garras de monstros sem coração. Apenas alguns descobrem sua identidade e conseguem chegar ao Acampamento Meio-Sangue, um acampamento de verão em Long Island dedicado ao treinamento de jovens semideuses.",
+        repository : "secaoVendas.html"
+    }
+];
+
+const fantasyArea = document.getElementById("fantasy");
+
+fantasy.forEach(project => {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `
+        <div class="project-image">
+            <img src="images/books/${project.image || 'placeholder.png'}" alt="imagem do livro">
+        </div>
+        <div class="text-content">
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <a href="${project.repository}" class="btn dark-button" target="_blank">Compre aqui!</a>
+        </div>
+    `;
+
+    fantasyArea.appendChild(item);
+});
+
+const romancesArea = document.getElementById("romances");
+
+romances.forEach(project => {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `
+        <div class="project-image">
+            <img src="images/books/${project.image || 'placeholder.png'}" alt="imagem do livro">
+        </div>
+        <div class="text-content">
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <a href="${project.repository}" class="btn dark-button" target="_blank">Compre aqui!</a>
+        </div>
+    `;
+
+    romancesArea.appendChild(item);
+});
+
+const bestSellersArea = document.getElementById("bests");
+
+bests.forEach(project => {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `
+        <div class="project-image">
+            <img src="images/books/${project.image || 'placeholder.png'}" alt="imagem do livro">
+        </div>
+        <div class="text-content">
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <a href="${project.repository}" class="btn dark-button" target="_blank">Compre aqui!</a>
+        </div>
+    `; 
+
+    bestSellersArea.appendChild(item);
+});
+
+const suggestBox = document.getElementById("suggest-box");
+
+suggestBox.forEach(input =>{
+    alert 
+})
+
+let suggestion = [];
+
+document.getElementById("suggest-btn").onClick = function(){
+    suggestion = document.getElementById("suggest-input").value;
+    document.getElementById("suggest-input").value = '';
+}
